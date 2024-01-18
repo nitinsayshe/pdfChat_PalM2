bb88cdd654e90e314445b866376c5e3688eff7a5
nitinsayshe <nitinsayshe@gmail.com>
2024-01-18 15:33:43 +0530

    demo3 first push with local faiss db

--- app.py
+++ app.py
@@ -2,57 +2,87 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import pprint
+import google.generativeai as genai
+from dotenv import load_dotenv
 import google.generativeai as palm
 from langchain.embeddings import GooglePalmEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.llms import GooglePalm
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
 
-os.environ['GOOGLE_API_KEY'] =  'AIzaSyA6QdTgid6CkbOEai4zMIip6OdQKlEXF6o'
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 
 def get_pdf_text(pdf_docs):
-    text=""
+    text = ""
     for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
+        pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+            text += page.extract_text()
+    return text
+
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
     chunks = text_splitter.split_text(text)
     return chunks
 
-def get_vector_store(text_chunks):
-    embeddings = GooglePalmEmbeddings()
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    return vector_store
 
-def get_conversational_chain(vector_store):
-    llm=GooglePalm()
-    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+def get_vector_store(text_chunks):
+    # embeddings = GooglePalmEmbeddings()
+
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+    # return vector_store
+
+
+def get_conversational_chain():
+    llm = GooglePalm()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    new_db = FAISS.load_local("faiss_index",embeddings)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=new_db.as_retriever(),
+        memory=memory)
     return conversation_chain
+
 
 def user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chatHistory = response['chat_history']
     for i, message in enumerate(st.session_state.chatHistory):
-        if i%2 == 0:
-            st.write("Human: ", message.content)
+        if i % 2 == 0:
+            st.write("Human : ", message.content)
         else:
-            st.write("Bot: ", message.content)
+            st.write("Bot : ", message.content)
+
 
 def main():
-    st.set_page_config("Chat with PDFs")
-    st.header("Chat with  PDF 💬")
+    st.set_page_config("wizzo demo3")
     user_question = st.text_input("Ask a Question from the PDF Files")
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chatHistory" not in st.session_state:
         st.session_state.chatHistory = None
+    styl = f"""
+      <style>
+          .stTextInput {{
+            position: fixed;
+            bottom: 0rem;
+            padding-bottom: 30px;
+            padding-top: 20px;  
+            background-color: #0E1117;
+            z-index: 100;
+          }}
+      </style>
+      """
+    st.markdown(styl, unsafe_allow_html=True)
     if user_question:
         user_input(user_question)
     with st.sidebar:
@@ -63,10 +93,9 @@ def main():
             with st.spinner("Processing"):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
-                vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
+                get_vector_store(text_chunks)
+                st.session_state.conversation = get_conversational_chain()
                 st.success("Done")
-
 
 
 if __name__ == "__main__":
